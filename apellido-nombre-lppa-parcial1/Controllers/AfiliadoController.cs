a14a707d4fef1da4c254b6ef45e6b18dc90f8748
Víctor Hugo Contreras <vcontreras@gmail.com>
2021-06-07 20:40:39 -0300

    Add List and Create Afiliado

--- apellido-nombre-lppa-parcial1/Controllers/AfiliadoController.cs
+++ apellido-nombre-lppa-parcial1/Controllers/AfiliadoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Data;
+using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,44 @@ namespace apellido_nombre_lppa_parcial1.Controllers
 {
     public class AfiliadoController : Controller
     {
-        // GET: Afiliado
+        BaseDataService<Afiliado> db;
+        public AfiliadoController()
+        {
+            db = new BaseDataService<Afiliado>();
+        }
+
+        [HttpGet]
         public ActionResult Index()
+        {
+            var list = db.Get();
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Afiliado model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Create(model);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Gestor de la Exception...");
+                }
+                
+            }
+            return View(model);
+        }
+
     }
 }
