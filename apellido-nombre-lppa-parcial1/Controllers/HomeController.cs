5b8c8af6d3c6001b1cc97abc1d944a2cf436df43
Víctor Hugo Contreras <vcontreras@gmail.com>
2021-06-07 19:40:41 -0300

    Add project files.

---
+++ apellido-nombre-lppa-parcial1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace apellido_nombre_lppa_parcial1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ apellido-nombre-lppa-parcial1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="apellido_nombre_lppa_parcial1.Global" Language="C#" %>
